140c7a4458d7b7a0d2e9a94409a7d14e372be089
przygoda <schmidt.multimedia@googlemail.com>
2022-05-03 08:55:52 +0000

    Tab anlegen / Spalten deklarieren/ Inhalte füllen / SELECT

--- 02_SQL_Tables/02a_db_table_SQL.sql
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -7,7 +7,30 @@
 /* Datenbanken auf Server anzeigen */
 SHOW DATABASES;
 
+/* DB boo löschen, falls vorhanden*/
+DROP DATABASE IF EXISTS boo;
+
 /* DB boo anlegen, falls noch nicht vorhanden*/
 CREATE DATABASE IF NOT EXISTS boo;
 
-SHOW DATABASES;
+/* DB auswählen */
+USE boo;
+
+CREATE TABLE test
+(
+	name VARCHAR(20),
+	age INT
+);
+
+/* Alle Tabellen in der DB anzeigen */
+SHOW TABLES;
+
+/* Struktur der Tabelle anzeigen */
+DESCRIBE test;
+
+/* ----- Daten ------- */
+INSERT INTO test(name,age) VALUES ("Grizabella",29);
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
+
+/* ---- Inhalte der Tabelle anzeigen ---- */
+SELECT * FROM test;
