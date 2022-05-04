b543149896299b1d064e6ce0bbaddd2dfd372cfa
przygoda <schmidt.multimedia@googlemail.com>
2022-05-04 09:04:34 +0000

    userPWD / unverschlüsselt

--- 03_user_DB/03a_db_userDB_SQL.sql
+++ 03_user_DB/03a_db_userDB_SQL.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS boo.users;
 CREATE TABLE IF NOT EXISTS boo.users
 (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    userName VARCHAR(20) NOT NULL,
+    userName VARCHAR(20) NOT NULL UNIQUE,
     familyName VARCHAR(20) NOT NULL,
     firstName VARCHAR(20) NOT NULL
 );
@@ -14,3 +14,13 @@ CREATE TABLE IF NOT EXISTS boo.users
 /* Struktur */
 DESCRIBE boo.users;
 
+/* Daten */
+INSERT INTO boo.users(userName,firstName,familyName)
+VALUES ("max","Max","Mütze");
+INSERT INTO boo.users(userName,firstName,familyName)
+VALUES ("maxine","Maxine","Mützerich");
+INSERT INTO boo.users(userName,firstName,familyName)
+VALUES ("maxl","Max","Mützerich");
+
+/* Inhalte : Ergebnistabelle */
+SELECT * FROM boo.users;
