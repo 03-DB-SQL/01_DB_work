797f2b7f63d7b8a7a707678259db0763448b436c
przygoda <schmidt.multimedia@googlemail.com>
2022-05-04 12:36:41 +0000

    DataImport 2 / 05 SELECT

--- 04_SQL_DataImport/04b_db_csv_import.sql
+++ 04_SQL_DataImport/04b_db_csv_import.sql
@@ -1,11 +1,11 @@
-/* Datenimport - csv mit ID */
+/* Datenimport - csv ohne ID */
 
 /* Tabelle cats*/
 DROP TABLE IF EXISTS boo.cats;
 
 CREATE TABLE IF NOT EXISTS boo.cats
 (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     cat_name VARCHAR(20) NOT NULL,
     fur_color VARCHAR(20) NOT NULL,
     age INT NOT NULL
@@ -15,11 +15,13 @@ CREATE TABLE IF NOT EXISTS boo.cats
 DESCRIBE boo.cats;
 
 -- CSV-Datei parsen
-LOAD DATA LOCAL INFILE "04_SQL_DataImport/data/cats_export.csv"
+LOAD DATA LOCAL INFILE "04_SQL_DataImport/data/cats_export_no_id.csv"
 INTO TABLE boo.cats
-FIELDS TERMINATED BY ";" 
+FIELDS TERMINATED BY "," 
 LINES TERMINATED BY "\n" 
 IGNORE 1 ROWS
+-- Daten in die richtigen Felder sortieren
+(cat_name,fur_color,age) 
 ;
 
 SELECT * FROM boo.cats;
