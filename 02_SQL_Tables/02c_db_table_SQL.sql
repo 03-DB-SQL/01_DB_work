640cf46f24ed5c2c37efe0356d4577a84179ac9a
przygoda <schmidt.multimedia@googlemail.com>
2022-05-04 06:44:52 +0000

    UNIQUE

--- 02_SQL_Tables/02c_db_table_SQL.sql
+++ 02_SQL_Tables/02c_db_table_SQL.sql
@@ -1,15 +1,10 @@
 /* ------  Strukturen ----- */
 
 /* 	 
-	NULL wird nicht mehr zugelassen
-    DEFAULT-Werte eintragen
+	Doppelte DS werden nicht mehr zugelassen
+    UNIQUE
 */
 
-/* DB boo löschen, falls vorhanden*/
-DROP DATABASE IF EXISTS boo;
-
-/* DB boo anlegen, falls noch nicht vorhanden*/
-CREATE DATABASE IF NOT EXISTS boo;
 
 /* DB auswählen */
 USE boo;
@@ -20,12 +15,10 @@ DROP TABLE IF EXISTS test;
 /* Tab. test anlegen, falls noch nicht vorhanden*/
 CREATE TABLE IF NOT EXISTS test
 (
-	name VARCHAR(20) NOT NULL DEFAULT "TBA",
+	name VARCHAR(20) NOT NULL UNIQUE DEFAULT "TBA",
 	age INT NOT NULL DEFAULT 0
 );
 
-/* Alle Tabellen in der DB anzeigen */
-#SHOW TABLES;
 
 /* Struktur der Tabelle anzeigen */
 DESCRIBE test;
@@ -35,9 +28,8 @@ INSERT INTO test(name,age) VALUES ("Grizabella",29);
 INSERT INTO test(age,name) VALUES (35,"Alonzo");
 INSERT INTO test VALUES ();
 
--- ABER: Doppelte Datensätze werden zugelassen !
-INSERT INTO test(age,name) VALUES (35,"Alonzo");
-INSERT INTO test(age,name) VALUES (35,"Alonzo");
+-- Doppelte Datensätze werden NICHT mehr zugelassen !
+INSERT INTO test(age,name) VALUES (35,"Alonzo der Coole");
 
 /* ---- Inhalte der Tabelle anzeigen ---- */
 SELECT * FROM test;
