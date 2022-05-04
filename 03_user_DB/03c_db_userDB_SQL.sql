67380273e525c67dd820fa5de40f3d46699ee3ec
przygoda <schmidt.multimedia@googlemail.com>
2022-05-04 10:02:28 +0000

    ALTER TABLE / SET / WHERE

--- 03_user_DB/03c_db_userDB_SQL.sql
+++ 03_user_DB/03c_db_userDB_SQL.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS boo.users
 );
 
 /* Struktur */
-DESCRIBE boo.users;
+#DESCRIBE boo.users;
 
 /* Daten */
 -- Nachteil: PWDs liegen offen! 
@@ -23,6 +23,24 @@ INSERT INTO boo.users(userName,firstName,familyName,userPwd)
 VALUES ("maxine","Maxine","Mützerich",SHA1("#7xD0"));
 INSERT INTO boo.users(userName,firstName,familyName,userPwd)
 VALUES ("maxl","Max","Mützerich",SHA1("user1234"));
+
+/* Inhalte : Ergebnistabelle */
+SELECT * FROM boo.users;
+
+/* Änderungen an bestehender Tabelle / Struktur*/
+ALTER TABLE
+    boo.users
+ADD    
+    userPLZ VARCHAR(5) NOT NULL DEFAULT "00000"
+;
+
+/* Struktur */
+DESCRIBE boo.users;
+
+/* Update vo Daten */
+UPDATE boo.users SET userPLZ = "70367" WHERE id = 1;
+UPDATE boo.users SET userPLZ = "70481" WHERE firstName = "Maxine";
+
 
 /* Inhalte : Ergebnistabelle */
 SELECT * FROM boo.users;
