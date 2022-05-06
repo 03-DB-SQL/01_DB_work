969edbb174fc85d96326de6fc82b88a9c4bf07fd
przygoda <schmidt.multimedia@googlemail.com>
2022-05-06 07:00:35 +0000

    Unscharfe Suche / LIKE

--- 05_SQL_SELECT/05_4_que_filter.sql
+++ 05_SQL_SELECT/05_4_que_filter.sql
@@ -10,6 +10,7 @@
 
 -- Eingrenzen/Filtern WHERE & AND/OR etc.
 -- Scharfe Suche
+/*
 SELECT
 	ticker AS "SYM",
     c_name AS "Unternehmen",
@@ -22,7 +23,7 @@ FROM stocks.ccc
 -- Einzeldaten / Strings / numerisch
 #WHERE sector = "Communication Services" -- spez. Sektor
 #WHERE industry = "Media" -- spez. Branche
-#WHERE payouts = 12 -- Wer zahlt monatlich?
+WHERE payouts = 12 -- Wer zahlt monatlich?
 
 -- Kombination durch AND
 #WHERE sector = "Communication Services" AND industry = "Entertainment"
@@ -30,7 +31,7 @@ FROM stocks.ccc
 -- Kombination durch AND / OR
 #WHERE sector = "Communication Services" AND (industry = "Entertainment" OR industry = "Media")
 -- Kombination durch AND / NOT
-WHERE sector = "Communication Services" AND NOT industry = "Media"
+#WHERE sector = "Communication Services" AND NOT industry = "Media"
 
 -- Sortierung
 ORDER BY industry DESC 
@@ -39,6 +40,45 @@ ORDER BY industry DESC
 -- Begrenzung
 LIMIT 20
 ;
+*/
+
+-- Eingrenzen/Filtern WHERE & LIKE + Parameter
+-- Unscharfe Suche 
+SELECT
+	ticker AS "SYM",
+    c_name AS Unternehmen,
+	industry Branche
+FROM stocks.ccc
+
+-- scharfe Suche nach Strings
+#WHERE industry = "Media"
+
+-- unschärfere Suchen --
+-- Branchenname beginnt mit ... , dahinter beliebige Chars
+#WHERE industry LIKE "Air%"
+-- Branchenname endet mit ... , davor beliebige Chars
+#WHERE industry LIKE "%ment"
+-- Branchenname enthaelt ...
+#WHERE industry LIKE "%ood%"
+
+-- Branchenname endet/beginnt mit ... , danach/davor/inmitten genau _ Char
+#WHERE industry LIKE "__dia"
+#WHERE industry LIKE "Med__"
+#WHERE industry LIKE "M___a"
+#WHERE industry LIKE "_ir%"
+#WHERE industry LIKE "_ood%"
+
+#WHERE industry LIKE "%ment"
+#WHERE industry LIKE "%ment" AND industry NOT LIKE "%ipment"
+#WHERE industry LIKE "%ment" AND industry NOT LIKE "%ipment" AND industry NOT LIKE "%tain%"
+
+
+ORDER BY industry ASC
+LIMIT 40
+;
+
+
+
 
 
 
