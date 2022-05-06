f9de0c57c62b423999456c9f851e210cdff1793e
przygoda <schmidt.multimedia@googlemail.com>
2022-05-06 07:16:44 +0000

    RegEx / RLIKE

--- 05_SQL_SELECT/05_4_que_filter.sql
+++ 05_SQL_SELECT/05_4_que_filter.sql
@@ -97,6 +97,17 @@ LIMIT 400 -- X Zeilen ab 0
 ;
 */
 
+-- Eingrenzen/Filtern WHERE & RegEx
+/*
+SELECT
+	c_name "Unternehmen"
+FROM stocks.ccc
+#WHERE c_name RLIKE "^[AZ]"  -- mit A oder Z beginnend
+WHERE c_name RLIKE "^[1-9]"  -- mit Ziffer beginnend
+ORDER BY c_name;
+*/
+
+
 
 
 
